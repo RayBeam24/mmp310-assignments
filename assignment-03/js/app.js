7a5590078b8dc3c9da193f8745292b684831d1b5
Raymond Hidalgo <raymond-hidalgo@hotmail.com>
2017-03-06 17:19:44 -0500

    add variables to both buttons

--- assignment-03/js/app.js
+++ assignment-03/js/app.js
@@ -1,2 +1,39 @@
 /*jslint browser, es6, single, for, devel, this */
 /*global window */
+var div = {
+    element: document.getElementsByName('div')[0],
+    height: 300,
+    width: 300
+}
+
+var divStyle = window.getComputedStyle(div.element);
+
+var buttonEnlarge = document.getElementById('enlarge-div');
+var buttonShrink = document.getElementById('shrink-div');
+
+function grow() {
+if (div.height < 500 && div.width < 500) {
+    div.height += 100;
+    div.element.style.height = div.height + "px";
+    
+    div.width += 100;
+    div.element.style.width = div.width + "px";
+   
+} else {
+    alert("Div is too large!");
+}
+}
+
+function shrink() {
+if (div.height > 100 && div.width > 100) {
+    div.height -= 100;
+    div.element.style.height = div.height + "px";
+    
+      div.height -= 100;
+    div.element.style.width = div.width + "px";
+    
+
+} else {
+    alert("Div is too small!");
+}
+}
